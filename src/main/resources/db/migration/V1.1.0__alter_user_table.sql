b56d8062faa645e7b29797178b47eb4802cf1f2c
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2020-11-25 12:54:26 +0330

    Formatted migration files for better readability.

--- src/main/resources/db/migration/V1.1.0__alter_user_table.sql
+++ src/main/resources/db/migration/V1.1.0__alter_user_table.sql
@@ -1,10 +1,9 @@
 alter table `user`
-add column profile varchar(255),
-add column about varchar(255),
-add column title varchar(50);
+    add column profile varchar(255),
+    add column about   varchar(255),
+    add column title   varchar(50);
 
 update `user`
-set 
-	profile = '/img/author.jpg',
-	about = 'Vivamus volutpat nibh ac sollicitudin imperdiet. Donec scelerisque lorem sodales odio ultricies, nec rhoncus ex lobortis. Vivamus tincid-unt sit amet sem id varius. Donec elementum aliquet tortor. Curabitur justo mi, efficitur sed eros alique.',
-    title = 'title';
+set profile = '/img/author.jpg',
+    about   = 'Vivamus volutpat nibh ac sollicitudin imperdiet. Donec scelerisque lorem sodales odio ultricies, nec rhoncus ex lobortis. Vivamus tincid-unt sit amet sem id varius. Donec elementum aliquet tortor. Curabitur justo mi, efficitur sed eros alique.',
+    title   = 'title';
